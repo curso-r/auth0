1355f98d65d9cb328a6368d09b032b737df9853f
jtrecenti <julio.trecenti@gmail.com>
2019-02-13 13:27:02 -0200

    update logout app

--- inst/logout/app.R
+++ inst/logout/app.R
@@ -3,16 +3,21 @@ library(auth0)
 library(shinyjs)
 
 # simple UI with action button
-# note that you must include shinyjs::useShinyjs() for this to work
-ui <- fluidPage(shinyjs::useShinyjs(), actionButton("logout_auth0", "Logout"))
+# you must include shinyjs::useShinyjs() to use logout button
+ui <- fluidPage(
+  shinyjs::useShinyjs(),
+  actionButton("logout_auth0", "Logout")  # logout button
+)
 
 # server with one observer that logouts
 server <- function(input, output, session) {
+
   observeEvent(input$logout_auth0, {
     # javascript code redirecting to correct url
     js <- auth0_logout_url()
     shinyjs::runjs(js)
   })
+
 }
 
 shinyAuth0App(ui, server)

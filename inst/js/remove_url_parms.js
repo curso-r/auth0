b66542d2962ecb5f4eb21cbc08c61fc2102baa1a
C. Lente <7017340+clente@users.noreply.github.com>
2019-06-13 16:34:42 -0300

    New solution to #22

--- inst/js/remove_url_parms.js
+++ inst/js/remove_url_parms.js
@@ -1,1 +1,1 @@
-var uri = window.location.toString();if (uri.indexOf('?') > 0) {var clean_uri = uri.substring(0, uri.indexOf('?'));window.history.replaceState({}, document.title, clean_uri);}
+var uri = window.location.toString();if (uri.indexOf('?') > 0) {var clean_uri = uri.replace(/code=.+&/gi, '');window.history.replaceState({}, document.title, clean_uri);}

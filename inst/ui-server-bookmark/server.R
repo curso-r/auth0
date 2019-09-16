68a73b59dff860663adbf82490aa3c3006a7c731
jtrecenti <julio.trecenti@gmail.com>
2019-09-15 21:50:45 -0300

    Bookmark and ui/server. Ref #5

--- inst/ui-server-bookmark/server.R
+++ inst/ui-server-bookmark/server.R
@@ -1,6 +1,10 @@
 library(shiny)
 library(auth0)
 
+# # run this before running the app:
+# options(shiny.port = 8080)
+# shiny::enableBookmarking(store = "server")
+
 server <- function(input, output, session) {
   output$out <- renderText({
     if (input$caps)

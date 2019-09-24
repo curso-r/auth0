636818da13869fe6fab85c995b240e9f26f0bd25
jtrecenti <julio.trecenti@gmail.com>
2019-09-23 21:47:10 -0300

    fix vignette

--- inst/ui-server-userinfo/server.R
+++ inst/ui-server-userinfo/server.R
@@ -1,9 +1,5 @@
 library(auth0)
 
 auth0_server(function(input, output, session) {
-
-  observe({
-    print(session$userData$auth0_info)
-  })
-
+  observe({ print(session$userData$auth0_info) })
 }, info = a0_info)
